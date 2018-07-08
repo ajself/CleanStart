22f735d405c373a4a39760efeab6c01fa58bb115
AJ Self <aj.self3@gmail.com>
2018-07-08 10:08:44 -0600

    Clean up view controller templates

--- Storyboards/Scene.xctemplate/UIViewController/___FILEBASENAME___Router.swift
+++ Storyboards/Scene.xctemplate/UIViewController/___FILEBASENAME___Router.swift
@@ -7,25 +7,26 @@
 
 import UIKit
 
-@objc protocol ___VARIABLE_sceneName___RoutingLogic
-{
+@objc protocol ___VARIABLE_sceneName___RoutingLogic {
   //func routeToSomewhere(segue: UIStoryboardSegue?)
 }
 
-protocol ___VARIABLE_sceneName___DataPassing
-{
+protocol ___VARIABLE_sceneName___DataPassing {
   var dataStore: ___VARIABLE_sceneName___DataStore? { get }
 }
 
-class ___VARIABLE_sceneName___Router: NSObject, ___VARIABLE_sceneName___RoutingLogic, ___VARIABLE_sceneName___DataPassing
-{
-  weak var viewController: ___VARIABLE_sceneName___ViewController?
+// MARK: - Object lifecycle
+
+class ___VARIABLE_sceneName___Router: NSObject, ___VARIABLE_sceneName___RoutingLogic, ___VARIABLE_sceneName___DataPassing {
+  // VIP properties
   var dataStore: ___VARIABLE_sceneName___DataStore?
-  
-  // MARK: - Routing
-  
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
-  //{
+  weak var viewController: ___VARIABLE_sceneName___ViewController?
+}
+
+// MARK: - Routing
+
+extension ___VARIABLE_sceneName___Router {
+  // func routeToSomewhere(segue: UIStoryboardSegue?) {
   //  if let segue = segue {
   //    let destinationVC = segue.destination as! SomewhereViewController
   //    var destinationDS = destinationVC.router!.dataStore!
@@ -37,19 +38,21 @@ class ___VARIABLE_sceneName___Router: NSObject, ___VARIABLE_sceneName___RoutingL
   //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
   //    navigateToSomewhere(source: viewController!, destination: destinationVC)
   //  }
-  //}
+  // }
+}
 
-  // MARK: - Navigation
-  
-  //func navigateToSomewhere(source: ___VARIABLE_sceneName___ViewController, destination: SomewhereViewController)
-  //{
+// MARK: - Navigation
+
+extension ___VARIABLE_sceneName___Router {
+  // func navigateToSomewhere(source: ___VARIABLE_sceneName___ViewController, destination: SomewhereViewController) {
   //  source.show(destination, sender: nil)
-  //}
-  
-  // MARK: - Passing data
-  
-  //func passDataToSomewhere(source: ___VARIABLE_sceneName___DataStore, destination: inout SomewhereDataStore)
-  //{
+  // }
+}
+
+// MARK: - Passing data
+
+extension ___VARIABLE_sceneName___Router {
+  // func passDataToSomewhere(source: ___VARIABLE_sceneName___DataStore, destination: inout SomewhereDataStore) {
   //  destination.name = source.name
-  //}
+  // }
 }
--- Storyboards/Scene.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
+++ Storyboards/Scene.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
@@ -7,34 +7,35 @@
 
 import UIKit
 
-protocol ___VARIABLE_sceneName___DisplayLogic: AnyObject
-{
+protocol ___VARIABLE_sceneName___DisplayLogic: AnyObject {
   func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel)
 }
 
-class ___VARIABLE_sceneName___ViewController: UIViewController, ___VARIABLE_sceneName___DisplayLogic
-{
+// MARK: - Object lifecycle
+
+class ___VARIABLE_sceneName___ViewController: UIViewController, ___VARIABLE_sceneName___DisplayLogic {
+  // View properties
+  // @IBOutlet weak var nameTextField: UITextField!
+
+  // VIP properties
   var interactor: ___VARIABLE_sceneName___BusinessLogic?
   var router: (NSObjectProtocol & ___VARIABLE_sceneName___RoutingLogic & ___VARIABLE_sceneName___DataPassing)?
-
-  // MARK: - Object lifecycle
   
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  {
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     setup()
   }
   
-  required init?(coder aDecoder: NSCoder)
-  {
+  required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     setup()
   }
+}
   
-  // MARK: - Setup
-  
-  private func setup()
-  {
+  // MARK: - VIP configuration
+ 
+extension ___VARIABLE_sceneName___ViewController {
+  private func setup() {
     let viewController = self
     let interactor = ___VARIABLE_sceneName___Interactor()
     let presenter = ___VARIABLE_sceneName___Presenter()
@@ -46,39 +47,43 @@ class ___VARIABLE_sceneName___ViewController: UIViewController, ___VARIABLE_scen
     router.viewController = viewController
     router.dataStore = interactor
   }
-  
-  // MARK: - Routing
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-  {
-    if let scene = segue.identifier {
-      let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-      if let router = router, router.responds(to: selector) {
-        router.perform(selector, with: segue)
-      }
-    }
+}
+
+// MARK: - Routing
+
+extension ___VARIABLE_sceneName___ViewController {
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    guard let scene = segue.identifier else { return }
+
+    let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
+
+    guard let router = router, router.responds(to: selector) else { return }
+
+    router.perform(selector, with: segue)
   }
-  
-  // MARK: - View lifecycle
-  
-  override func viewDidLoad()
-  {
+}
+
+// MARK: - View lifecycle
+
+extension ___VARIABLE_sceneName___ViewController {
+  override func viewDidLoad() {
     super.viewDidLoad()
     doSomething()
   }
+}
+
+// MARK: - Inputs
+
+extension ___VARIABLE_sceneName___ViewController {
+  // MARK: - Something
   
-  // MARK: - Do something
-  
-  //@IBOutlet weak var nameTextField: UITextField!
-  
-  func doSomething()
-  {
+  func doSomething() {
     let request = ___VARIABLE_sceneName___.Something.Request()
+
     interactor?.doSomething(request: request)
   }
-  
-  func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel)
-  {
-    //nameTextField.text = viewModel.name
+
+  func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel) {
+    // nameTextField.text = viewModel.name
   }
 }
