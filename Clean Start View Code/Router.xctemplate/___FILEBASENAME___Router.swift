697c7dc02091e9b241386b66878993530ab45ff8
AJ Self <aj.self3@gmail.com>
2018-07-08 11:19:49 -0600

    Reformat router

--- Clean Start View Code/Router.xctemplate/___FILEBASENAME___Router.swift
+++ Clean Start View Code/Router.xctemplate/___FILEBASENAME___Router.swift
@@ -7,49 +7,46 @@
 
 import UIKit
 
-@objc protocol ___VARIABLE_sceneName___RoutingLogic
-{
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
+@objc public protocol ___VARIABLE_sceneName___RoutingLogic {
+  func routeToSomewhere()
 }
 
-protocol ___VARIABLE_sceneName___DataPassing
-{
+public protocol ___VARIABLE_sceneName___DataPassing {
   var dataStore: ___VARIABLE_sceneName___DataStore? { get }
 }
 
-class ___VARIABLE_sceneName___Router: NSObject, ___VARIABLE_sceneName___RoutingLogic, ___VARIABLE_sceneName___DataPassing
-{
-  weak var viewController: ___VARIABLE_sceneName___ViewController?
-  var dataStore: ___VARIABLE_sceneName___DataStore?
-  
-  // MARK: - Routing
-  
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
-  //{
-  //  if let segue = segue {
-  //    let destinationVC = segue.destination as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //  } else {
-  //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-  //    let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //    navigateToSomewhere(source: viewController!, destination: destinationVC)
-  //  }
-  //}
+// MARK: - Object lifecycle
 
-  // MARK: - Navigation
-  
-  //func navigateToSomewhere(source: ___VARIABLE_sceneName___ViewController, destination: SomewhereViewController)
-  //{
+public class ___VARIABLE_sceneName___Router: NSObject, ___VARIABLE_sceneName___RoutingLogic, ___VARIABLE_sceneName___DataPassing {
+  // VIP properties
+  public var dataStore: ___VARIABLE_sceneName___DataStore?
+  weak public var viewController: ___VARIABLE_sceneName___ViewController?
+}
+
+// MARK: - Routing
+
+extension ___VARIABLE_sceneName___Router {
+  public func routeToSomewhere() {
+    // let destinationVC = SomewhereViewController()
+    // let destinationDS = destinationVC.router!.dataStore!
+
+    // passDataToSomewhere(source: dataStore!, destination: &destinationDS)
+    // navigateToSomewhere(source: viewController!, destination: destinationVC)
+  }
+}
+
+// MARK: - Navigation
+
+extension ___VARIABLE_sceneName___Router {
+  // internal func navigateToSomewhere(source: ___VARIABLE_sceneName___ViewController, destination: SomewhereViewController) {
   //  source.show(destination, sender: nil)
-  //}
-  
-  // MARK: - Passing data
-  
-  //func passDataToSomewhere(source: ___VARIABLE_sceneName___DataStore, destination: inout SomewhereDataStore)
-  //{
+  // }
+}
+
+// MARK: - Passing data
+
+extension ___VARIABLE_sceneName___Router {
+  // internal func passDataToSomewhere(source: ___VARIABLE_sceneName___DataStore, destination: inout SomewhereDataStore) {
   //  destination.name = source.name
-  //}
+  // }
 }
