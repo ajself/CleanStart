2ce42856091e889e7158830209dccd9e75e798a4
AJ Self <aj.self3@gmail.com>
2018-07-08 11:32:29 -0600

    UICollectionViewController scene

--- Clean Start View Code/Scene.xctemplate/UICollectionViewController/___FILEBASENAME___Presenter.swift
+++ Clean Start View Code/Scene.xctemplate/UICollectionViewController/___FILEBASENAME___Presenter.swift
@@ -7,20 +7,23 @@
 
 import UIKit
 
-protocol ___VARIABLE_sceneName___PresentationLogic
-{
+public protocol ___VARIABLE_sceneName___PresentationLogic {
   func presentSomething(response: ___VARIABLE_sceneName___.Something.Response)
 }
 
-class ___VARIABLE_sceneName___Presenter: ___VARIABLE_sceneName___PresentationLogic
-{
-  weak var viewController: ___VARIABLE_sceneName___DisplayLogic?
+public class ___VARIABLE_sceneName___Presenter: ___VARIABLE_sceneName___PresentationLogic {
+  // VIP properties
+  weak public var viewController: ___VARIABLE_sceneName___DisplayLogic?
+}
+
+// MARK: - Inputs
+
+extension ___VARIABLE_sceneName___Presenter {
+  // MARK: - Something
   
-  // MARK: - Do something
-  
-  func presentSomething(response: ___VARIABLE_sceneName___.Something.Response)
-  {
+  public func presentSomething(response: ___VARIABLE_sceneName___.Something.Response) {
     let viewModel = ___VARIABLE_sceneName___.Something.ViewModel()
+    
     viewController?.displaySomething(viewModel: viewModel)
   }
 }
--- Clean Start View Code/Scene.xctemplate/UICollectionViewController/___FILEBASENAME___ViewController.swift
+++ Clean Start View Code/Scene.xctemplate/UICollectionViewController/___FILEBASENAME___ViewController.swift
@@ -7,34 +7,35 @@
 
 import UIKit
 
-protocol ___VARIABLE_sceneName___DisplayLogic: AnyObject
-{
+public protocol ___VARIABLE_sceneName___DisplayLogic: AnyObject {
   func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel)
 }
 
-class ___VARIABLE_sceneName___ViewController: UICollectionViewController, ___VARIABLE_sceneName___DisplayLogic
-{
-  var interactor: ___VARIABLE_sceneName___BusinessLogic?
-  var router: (NSObjectProtocol & ___VARIABLE_sceneName___RoutingLogic & ___VARIABLE_sceneName___DataPassing)?
+// MARK: - Object lifecycle
 
-  // MARK: - Object lifecycle
+public class ___VARIABLE_sceneName___ViewController: UICollectionViewController, ___VARIABLE_sceneName___DisplayLogic {
+  // View properties
+  // internal let nameTextField = UITextField()
+
+  // VIP properties
+  public var interactor: ___VARIABLE_sceneName___BusinessLogic?
+  public var router: (___VARIABLE_sceneName___RoutingLogic & ___VARIABLE_sceneName___DataPassing)?
   
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  {
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     setup()
   }
   
-  required init?(coder aDecoder: NSCoder)
-  {
+  required public init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     setup()
   }
+}
   
-  // MARK: - Setup
-  
-  private func setup()
-  {
+  // MARK: - VIP configuration
+ 
+extension ___VARIABLE_sceneName___ViewController {
+  private func setup() {
     let viewController = self
     let interactor = ___VARIABLE_sceneName___Interactor()
     let presenter = ___VARIABLE_sceneName___Presenter()
@@ -46,39 +47,45 @@ class ___VARIABLE_sceneName___ViewController: UICollectionViewController, ___VAR
     router.viewController = viewController
     router.dataStore = interactor
   }
-  
-  // MARK: - Routing
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-  {
-    if let scene = segue.identifier {
-      let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-      if let router = router, router.responds(to: selector) {
-        router.perform(selector, with: segue)
-      }
-    }
-  }
-  
-  // MARK: - View lifecycle
-  
-  override func viewDidLoad()
-  {
+}
+
+// MARK: - View lifecycle
+
+extension ___VARIABLE_sceneName___ViewController {
+  override public func viewDidLoad() {
     super.viewDidLoad()
     doSomething()
   }
+}
+
+// MARK: - View configuration
+
+extension ___VARIABLE_sceneName___ViewController {
+  public func setupViewHierarchy() {
+
+  }
+
+  public func setupViews() {
+
+  }
+
+  public func setupConstraints() {
+
+  }
+}
+
+// MARK: - Inputs
+
+extension ___VARIABLE_sceneName___ViewController {
+  // MARK: - Something
   
-  // MARK: - Do something
-  
-  //@IBOutlet weak var nameTextField: UITextField!
-  
-  func doSomething()
-  {
+  internal func doSomething() {
     let request = ___VARIABLE_sceneName___.Something.Request()
+
     interactor?.doSomething(request: request)
   }
-  
-  func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel)
-  {
-    //nameTextField.text = viewModel.name
+
+  public func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel) {
+    // nameTextField.text = viewModel.name
   }
 }
