d334db0385d9727e54cd0fd99ea97fc5ac844adc
AJ Self <aj.self3@gmail.com>
2018-07-08 10:17:30 -0600

    Update unit test templates to utilize extensions and 1TBS

--- Storyboards/Unit Tests.xctemplate/___FILEBASENAME___PresenterTests.swift
+++ Storyboards/Unit Tests.xctemplate/___FILEBASENAME___PresenterTests.swift
@@ -5,51 +5,50 @@
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 
-@testable import ___PROJECTNAMEASIDENTIFIER___
 import XCTest
 
-class ___VARIABLE_sceneName___PresenterTests: XCTestCase
-{
+@testable import ___PROJECTNAMEASIDENTIFIER___
+
+class ___VARIABLE_sceneName___PresenterTests: XCTestCase {
   // MARK: - Subject under test
   
   var sut: ___VARIABLE_sceneName___Presenter!
   
   // MARK: - Test lifecycle
   
-  override func setUp()
-  {
+  override func setUp() {
     super.setUp()
     setup___VARIABLE_sceneName___Presenter()
   }
   
-  override func tearDown()
-  {
+  override func tearDown() {
     super.tearDown()
   }
   
-  // MARK: - Test setup
-  
-  func setup___VARIABLE_sceneName___Presenter()
-  {
+// MARK: - Test setup
+
+extension ___VARIABLE_sceneName___PresenterTests {
+  func setup___VARIABLE_sceneName___Presenter() {
     sut = ___VARIABLE_sceneName___Presenter()
   }
+}
   
-  // MARK: - Test doubles
-  
-  class ___VARIABLE_sceneName___DisplayLogicSpy: ___VARIABLE_sceneName___DisplayLogic
-  {
+// MARK: - Test doubles
+
+extension ___VARIABLE_sceneName___PresenterTests {
+  class ___VARIABLE_sceneName___DisplayLogicSpy: ___VARIABLE_sceneName___DisplayLogic {
     var displaySomethingCalled = false
     
-    func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel)
-    {
+    func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel) {
       displaySomethingCalled = true
     }
   }
-  
-  // MARK: - Tests
-  
-  func testPresentSomething()
-  {
+}
+
+// MARK: - Tests
+
+extension ___VARIABLE_sceneName___PresenterTests {
+  func testPresentSomething() {
     // Given
     let spy = ___VARIABLE_sceneName___DisplayLogicSpy()
     sut.viewController = spy
